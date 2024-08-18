63ce80155c241d9a530c835dcbd9a6a0388dcd93
Aditya Upadhyay <adityaupadhyay727@gmail.com>
2024-08-18 07:20:37 +0530

    Setting up basic project layout

---
+++ app/dashboard/page.js
@@ -0,0 +1,22 @@
+import Dashboard from "@/components/Dashboard";
+import Login from "@/components/Login";
+import Main from "@/components/Main";
+
+export const metadata = {
+    title: "Broodl ⋅ Dashboard",
+  };
+
+export default function DashboardPage() {
+
+  const isAuthenticated = false;
+
+  let children = (<Login />)
+
+  if(isAuthenticated) {
+    children = (<Dashboard/>)
+  }
+
+  return (
+    <Main>{children}</Main>
+  )
+}
