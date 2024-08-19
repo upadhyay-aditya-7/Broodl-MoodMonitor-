f546d2d1638dfef0d5026d64ca35eac4e61e717f
Aditya Upadhyay <adityaupadhyay727@gmail.com>
2024-08-19 19:20:32 +0530

    Final Code

--- components/Login.js
+++ components/Login.js
@@ -38,7 +38,7 @@ export default function Login() {
   return (
     <div className='flex flex-col flex-1 justify-center items-center gap-4'>
       <h3 className={'text-4xl sm:text:5xl md:text-6xl ' + fugaz.className}>{isRegister ? 'Register':'Log In'}</h3>
-      <p>You're one step away!</p>
+      <p>You&#39;re one step away!</p>
       <input value={email} onChange={(e) => {
         setEmail(e.target.value)
       }} className='max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border-2 border-solid border-indigo-400 rounded-full outline-none' placeholder='Enter your email'/>
